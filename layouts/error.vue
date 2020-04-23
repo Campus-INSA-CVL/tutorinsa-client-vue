ce4633fe5941737c624fbcd51c3670e805d43a91
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-04-24 01:29:51 +0200

    add the error glitch effect

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,10 @@
-<template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+<template lang="pug">
+  section
+    v-row
+      v-col(align="center")
+        div(v-if="error.statusCode === 404", :data-text="pageNotFound").glitch {{pageNotFound}}
+        h1(v-else) {{otherError}}
+    nuxt-link(to="/") Home page
 </template>
 
 <script>
@@ -37,8 +32,62 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  font-size: 20px;
+<style lang="scss" scoped>
+@import '~vuetify/src/styles/styles.sass';
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap');
+
+.glitch {
+  font-size: 6rem;
+  line-height: 6rem;
+  font-family: Nunito, sans-serif;
+  position: relative;
+}
+@keyframes noise-anim {
+  $steps: 20;
+  @for $i from 0 through $steps {
+    #{percentage($i*(1/$steps))} {
+      $top: random(100);
+      $bottom: random(101 - $top);
+      clip-path: inset(#{$top} + '%' 0 #{$bottom} + '%' 0);
+    }
+  }
+}
+.glitch::after {
+  content: attr(data-text);
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 2px;
+  right: 0;
+  text-align: center;
+  text-shadow: -1px 0 red;
+  top: 0;
+  background: transparent;
+  overflow: hidden;
+  animation: noise-anim 1.2s infinite linear alternate-reverse;
+}
+@keyframes noise-anim-2 {
+  $steps: 20;
+  @for $i from 0 through $steps {
+    #{percentage($i*(1/$steps))} {
+      $top: random(100);
+      $bottom: random(101 - $top);
+      clip-path: inset(#{$top} + '%' 0 #{$bottom} + '%' 0);
+    }
+  }
+}
+.glitch::before {
+  content: attr(data-text);
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: -2px;
+  right: 0;
+  text-align: center;
+  text-shadow: 2px 0 rgb(0, 68, 255);
+  top: 0;
+  background: transparent;
+  overflow: hidden;
+  animation: noise-anim-2 12s infinite linear alternate-reverse;
 }
 </style>
