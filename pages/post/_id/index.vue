0b96f93d2a253c8ac20bdc3a0f5498f82eac875d
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-04-24 13:11:51 +0200

    update package

--- pages/post/_id/index.vue
+++ pages/post/_id/index.vue
@@ -9,8 +9,6 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 
-// import { EventBus } from '@/utils/event-bus'
-
 import Post from '@/components/Post/Post'
 import AboutCreator from '@/components/Post/AboutCreator'
 
@@ -20,31 +18,13 @@ export default {
     'post-app': Post,
     'about-creator-app': AboutCreator
   },
-  // async fetch() {
-  //   try {
-  //     let response = await this.getPost(this.$route.params.id)
-  //     if (response === null) {
-  //       response = await this.fetchPost(this.$route.params.id)
-  //     }
-  //     this.post = response
-  //   } catch (error) {
-  //     EventBus.$emit('snackEvent', {
-  //       color: 'error',
-  //       message: 'Une erreur est survenue lors du chargement des postes',
-  //       active: true,
-  //       close: true
-  //     })
-  //   }
-  // },
   asyncData(context) {
-    console.debug('context:', context)
     const { store, params, error } = context
     const post = store.getters['posts/get'](params.id)
     if (!post) {
       return store
         .dispatch('posts/get', params.id)
         .then((res) => {
-          console.debug('res:', res)
           return { post: res }
         })
         .catch((e) => {
@@ -59,7 +39,6 @@ export default {
       return { post }
     }
   },
-  // fetchOnServer: false,
   data() {
     return {
       transition: 'fade-transition'
