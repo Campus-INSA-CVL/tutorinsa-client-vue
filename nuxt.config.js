dc803c4192880c732236112e10de7382da538f5e
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-19 02:56:37 +0200

    add content

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: ''
+        content: 'this is the content'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
